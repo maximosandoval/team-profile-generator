16329b3d07e279c472646f859dc35f7712bc13ab
Maximo Sandoval <80641742+maximosandoval@users.noreply.github.com>
2021-07-13 12:17:51 -0600

    Updated Generate
    1. Added in additional code so I could break the file.

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,60 +1,63 @@
-//Need to put in this file the HTML in template literals
-//Loop thru with for
+//Place HTML template literals
+//Loop thru statements for each position
 //Add ${answer} the end of HTML where the data goes
+const fs = require("fs");
 
-`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css" />
-    <title>Maximo Sandoval Team Directory</title>
-  </head>
-  <body>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12 jumbotron md-3 mainheader">
-      <h1>Team Directory</h1>
-    </div>
-    </div>
-    <div class="container">
-      <div class="row">
-      <div class="col-12 d-flex justify-content-center">
-        <br><br>
-      <!--Start Manager Card-->
-        <div class="card">
+//function to add employee
+function addEmployee(arr) {
+  for (i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    console.log(arr[i].getRole());
+    if (arr[i].getRole() == "Manager") {
+      `<div class="card">
        <div class="card-header">
         <h2 class="card-fullname"><i class="fas fa-mug-hot mr-2"></i>Manager</h2>
         </div>   
         <div class="card-body">
         <ul class="list-main">
-          <li class="list-id">ID:${employee.id}</li>
-          <li class="list-id">Email:}<a href="mailto:${employee.email}"></a></li>
-          <li class="list-id">Office Number:</li>
+          <li class="list-id">ID:${manager.id}</li>
+          <li class="list-id">Email:${manager.email}<a href="mailto:"></a></li>
+          <li class="list-id">Office Number:${manager.officenumber}</li>
         </ul>
       </div>
-      </div>
-      <!--END Manager Card-->
-
-      <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
-</html>`;
-
-//Needs to be seperate item
-` <!--Start Intern Card-->
-<div class="card">
+      </div>`;
+      break;
+    }
+    if (arr[i].getRole() == "Engineer") {
+      `<div class="card">
+        <div class="card-header">
+         <h2 class="card-fullname"><i class="fas fa-mug-hot mr-2"></i>Engineer</h2>
+         </div>   
+         <div class="card-body">
+         <ul class="list-main">
+           <li class="list-id">ID:${engineer.id}</li>
+           <li class="list-id">Email:<a href="mailto:${engineer.email}">${engineer.email}</a></li>
+           <li class="list-id">GitHub:${engineer.github}</li>
+         </ul>
+       </div>
+       </div>`;
+      break;
+    }
+    if (arr[i].getRole() == "Intern") {
+      renderIntern(arr[i]);
+      ` <div class="card">
  <div class="card-header">
   <h2 class="card-fullname"><i class="fas fa-mug-hot mr-2"></i>Intern</h2>
   </div>   
   <div class="card-body">
   <ul class="list-main">
-    <li class="list-id">ID:</li>
-    <li class="list-id">Email:<a href="mailto:"></a></li>
-    <li class="list-id">School:</li>
+    <li class="list-id">ID:${intern.id}</li>
+    <li class="list-id">Email:<a href="mailto:${intern.email}"></a>${intern.email}</li>
+    <li class="list-id">School:${intern.school}</li>
   </ul>
 </div>
-</div>
-<!--End Intern Card-->`;
+</div>`;
+    }
+    break;
+  }
+}
+fs.writeFileSync("./dist/index.html", renderHTML, (err) => {
+  if (err) throw err;
+});
+
+module.exports = generateHTML;
